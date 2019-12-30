fb98757a2bcd4deb08417186b46fa91bd8533b4e
gyb333 <876368840@qq.com>
2019-12-30 23:26:14 +0800

    test

--- src/algorithm/maze/maze.go
+++ src/algorithm/maze/maze.go
@@ -3,14 +3,13 @@ package main
 import (
 	"fmt"
 	"os"
-	"sort"
-)
+	)
 
 func main() {
 	maze := readMaze("src/algorithm/maze/maze.in")
-	start:=Point{0, 0,0}
-	end:=Point{len(maze) - 1, len(maze[0]) - 1,0}
-	steps := walk(maze, start, end)
+	start:=Point{0, 0}
+	end:=Point{len(maze) - 1, len(maze[0]) - 1}
+	steps,endNode := walk(maze, start, end)
 	for _, row := range steps {
 		for _, val := range row {
 			fmt.Printf("%3d ", val)
@@ -18,8 +17,9 @@ func main() {
 		fmt.Println()
 	}
 
-
-	walkLine(steps,start,end)
+	for node:=endNode;node!=nil;node=node.preNode{
+		fmt.Println(node.row,node.colum,node.val)
+	}
 
 
 
@@ -44,11 +44,10 @@ func readMaze(filename string) (maze [][]int) {
 
 type Point struct {
 	row, colum int
-	val int
 }
 
 func (p Point) add(dir Point) Point {
-	return Point{p.row + dir.row, p.colum + dir.colum,p.val+dir.val}
+	return Point{p.row + dir.row, p.colum + dir.colum}
 }
 
 func (p Point) getValue(grid [][]int) (int, bool) {
@@ -63,109 +62,58 @@ func (p Point) getValue(grid [][]int) (int, bool) {
 	return grid[p.row][p.colum], true
 }
 
-
-
 var dirs = [4]Point{
-	{-1, 0,-1}, {0, -1,-1}, {1, 0,-1}, {0, 1,-1},
+	{-1, 0}, {0, -1}, {1, 0}, {0, 1},
 }
 
-
-
-func walk(maze [][]int, start, end Point) (steps [][]int) {
+func walk(maze [][]int, start, end Point) (steps [][]int,endNode *Node) {
 	steps = make([][]int, len(maze))
 	for i := range steps {
 		steps[i] = make([]int, len(maze[i]))
 	}
-
-
-	points := []Point{start}
-
+	//points := []Point{start}
+	points := []*Node{creatNode(start,0)}
 	for len(points) > 0 {
 		cur := points[0]
-		if cur == end {
+		if cur.Point == end {
+			endNode=cur
 			break
 		}
 		curSteps, _ := cur.getValue(steps)
-
 		points = points[1:]
-
 		for _, dir := range dirs {
 			next := cur.add(dir)
 			if val, ok := next.getValue(maze); !ok || val == 1 {
 				continue
 			}
-
-			nextStep, ok := next.getValue(steps)
-			if !ok || nextStep != 0 {
+			if nextStep, ok := next.getValue(steps); !ok || nextStep != 0{
 				continue
 			}
-
 			if next == start {
 				continue
 			}
-
-			steps[next.row][next.colum]  =curSteps + 1
-
-			points = append(points, next)
+			val :=curSteps + 1
+			steps[next.row][next.colum]  =val
+			nextNode:=creatNode(next,val)
+			nextNode.preNode=cur
+			points = append(points, nextNode)
 		}
-
-
-
 	}
 	return
 }
 
-type pointSlice  []Point
-
-func (p pointSlice) Len() int           { return len(p) }
-func (p pointSlice) Less(i, j int) bool {
-	if p[i].val == p[j].val{
-		if p[i].row==p[j].row{
-			return p[i].colum<p[j].colum
-		}
-		return p[i].row<p[j].row
-	}
-	return p[i].val < p[j].val
-
-
+type Node struct {
+	Point
+	val int
+	preNode *Node
 }
-func (p pointSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 
-
-
-func walkLine(steps [][]int,start, end Point){
-	points :=make(pointSlice,0)
-
-	for i,row :=range steps{
-		for j,val:=range row{
-			if steps[i][j]>0{
-				points=append(points,Point{i,j,val})
-			}
-		}
-	}
-	end =points[len(points)-1]
-	sort.Sort(sort.Reverse(points))
-
-	for len(points)>0{
-		cur := points[0]
-		points=points[1:]
-		if cur ==end{
-			fmt.Println(cur)
-			//for _, dir := range dirs {
-			//	//next := cur.add(dir)
-			//	//points.
-			//
-			//
-			//}
-		}
-
-
-		//for _,v :=range points{
-		//
-		//}
-	}
-
-
-
-	fmt.Println(points)
+func creatNode(p Point,val int) *Node  {
+	return &Node{Point:Point{p.row,p.colum},val:val,preNode: nil}
 }
+
+
+
+
+
+
